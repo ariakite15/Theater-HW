034b800130ca979666af42518f8abfd16fadd444
cap <cap@caps-MacBook-Pro.local>
2016-04-15 14:43:00 +0900

    initial commit

---
+++ Theater.java
@@ -0,0 +1,220 @@
+package Theater_April_15;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * Created by cap on 4/15/16.
+ */
+package Movie;
+        import java.util.InputMismatchException;
+        import java.util.Scanner;
+
+
+class SeatMap2{
+
+    private int _row, _column;
+    private Seat2 _seat[][];
+    Scanner input;
+
+    SeatMap2(int row, int column){ //constructor
+        _row = row;
+        _column= column;
+        _seat = new Seat2[_row][_column];
+        seat_allocate();
+        input = new Scanner(System.in);
+    }
+
+    private void seat_allocate() {
+        for (int row_idx = 0; row_idx < _row; row_idx++) {
+            for (int col_idx = 0; col_idx < _column; col_idx++)
+                _seat[row_idx][col_idx] = new Seat2(); //저장 공간 만들기
+        }
+    }
+
+    void reserve(){
+        String _name;
+        int _reserveSeat;
+        String _seatLetter;
+        int _seatNumber;
+        int _seatNumber2;
+
+        System.out.print("Name: ");
+        _name = input.next();
+
+
+        try{
+            System.out.print("Number of seats: ");
+            _reserveSeat = input.nextInt();
+
+
+            System.out.print("Seat row alphabet: ");
+            _seatLetter = input.next();
+
+            if(_seatLetter.length() > 1 || _seatLetter.charAt(0) < 'A' || _seatLetter.charAt(0) > 'E'){
+                throw new InputMismatchException();
+            }
+
+            if(1 < _reserveSeat && _reserveSeat <= 9){
+                System.out.print("Reserve seat number from: ");
+                _seatNumber = input.nextInt();
+                System.out.print("to: ");
+                _seatNumber2 = input.nextInt();
+
+                if(_seatNumber > _seatNumber2){
+                    for(int column_indx = _seatNumber; column_indx <= _seatNumber2; _seatNumber++){
+                        if (_seat[_seatLetter.charAt(0) - 'A'][column_indx].isOccupied()){
+                            System.out.print("You cannot reserve the seats in this area.");
+                            break;
+                        }
+                    }
+                    for(int row_indx = 0; row_indx < _row; row_indx++) {
+                        for (int column_indx = 0; column_indx < _column; column_indx++){
+                            
+
+                        }
+                    }
+
+                    // how to show the reservation changes on the seating chart?
+
+                }
+                else{
+                    for(int column_indx = _seatNumber2; column_indx >= _seatNumber; column_indx--){
+                        if(_seat[_seatLetter.charAt(0) - 'A'][column_indx].isOccupied()){
+                            System.out.print("You cannot reserve the seats in this area.");
+                            break;
+                        }
+                    }
+                    // how to how to show the reservation changes on the seating chart?
+                }
+
+
+            }
+            else if(_reserveSeat == 1) {
+                System.out.print("Seat number: ");
+                _seatNumber = input.nextInt();
+                // how to show the reservation changes on the seating chart? do you call isOccupied()?
+            }
+            else
+                System.out.println("You can only reserve betweeen 1-9 seats.");
+
+        }
+        catch(InputMismatchException e){
+            System.out.println("You have entered either an invalid number or alphabet.");
+        }
+
+    }
+
+    void view(){
+        System.out.println();
+        //System.out.println(Seat2.getName(reserver));
+
+    }
+
+    void cancel(){
+
+    }
+
+    void showMap(){
+        System.out.print(" " );
+        for(int column_indx = 1; column_indx <= _column; column_indx++)
+            System.out.printf("%4d", column_indx);
+        System.out.println();
+
+        for(int row_indx = 0; row_indx < _row; row_indx++){
+            System.out.print((char)(row_indx + 'A') + ":");
+            for(int column_indx = 0; column_indx < _column; column_indx++){
+                System.out.print(" [");
+                if(_seat[row_indx][column_indx].isOccupied())
+                    System.out.print(" 0]");
+                else
+                    System.out.print(" ]");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
+
+class Seat2{
+    static boolean reserved;
+
+    Seat2(){
+        reserved = false;
+
+    }
+    String getName(String reserver){
+        return reserver;
+
+    }
+
+    void cancel(){
+
+    }
+
+    void setName(){
+
+    }
+
+    static boolean isOccupied(){
+        return reserved;
+    }
+
+    void match(){
+
+    }
+}
+
+public class Theater {
+    final static int SEAT_ROW = 5;
+    final static int SEAT_COLUMN = 9;
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        SeatMap2 myTheater = new SeatMap2(SEAT_ROW, SEAT_COLUMN);
+        String _input;
+
+
+        while(true) {
+            myTheater.showMap();
+            System.out.print("(R)eserve, (V)iew, (C)ancel, (Q)uit: ");
+            _input = input.next();
+
+            switch(_input){
+                case "r":
+                case "R":
+                case "reserve":
+                case "Reserve":
+                    myTheater.reserve();
+                    break;
+                case "v":
+                case "V":
+                case "view":
+                case "View":
+                    myTheater.view();
+                    break;
+                case "c":
+                case "C":
+                case "cancel":
+                case "Cancel":
+                    myTheater.cancel();
+                    break;
+                case "q":
+                case "Q":
+                case "quit":
+                case "Quit":
+                    System.exit(0);
+                default:
+                    System.out.println("You have inputted an invalid option.");
+                    break;
+
+            }
+
+
+        }
+
+    }
+
+}
